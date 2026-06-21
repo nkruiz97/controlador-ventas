ef4c0674e39630fb109e50968e56921b3e3a8e62
nkruiz97 <nkruiz1997@gmail.com>
2026-06-21 04:35:20 -0300

    Fix: Corrección de bucle infinito en rutas de URLs

--- ventas/urls.py
+++ ventas/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('', views.panel_ventas, name='panel_ventas'),
     path('nueva/', views.crear_venta_ajax, name='crear_venta'),
     path('venta/<int:venta_id>/pdf/', views.descargar_pdf_venta, name='descargar_pdf_venta'),
-    path('ventas/', include('ventas.urls')),
 ]
