b7e7c0a3a057b500df03e95f492b170b12becb8c
nkruiz97 <nkruiz1997@gmail.com>
2026-06-21 04:30:54 -0300

    Conexión de las urls de ventas en el urls principal

--- ventas/urls.py
+++ ventas/urls.py
@@ -1,9 +1,10 @@
 # ventas/urls.py
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.panel_ventas, name='panel_ventas'),
     path('nueva/', views.crear_venta_ajax, name='crear_venta'),
     path('venta/<int:venta_id>/pdf/', views.descargar_pdf_venta, name='descargar_pdf_venta'),
+    path('ventas/', include('ventas.urls')),
 ]
